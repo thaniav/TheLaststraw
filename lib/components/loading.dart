45d4d911a5f2e6f0d3c3c63de36de41e40eb1445
thaniav <61160291+thaniav@users.noreply.github.com>
2020-02-29 23:39:40 +0530

    Sign in, Register Loading screen

--- lib/components/loading.dart
+++ lib/components/loading.dart
@@ -3,6 +3,7 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
