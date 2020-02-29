973b20fadb8a84bc78e1a08b9f00baecb9bb78dd
thaniav <61160291+thaniav@users.noreply.github.com>
2020-02-29 22:17:09 +0530

    Sign In anonymously and logout

--- lib/Pages/authenticate/SignInPage.dart
+++ lib/Pages/authenticate/SignInPage.dart
@@ -172,6 +172,7 @@ class _MyHomePageState extends State<SignInPage> {
                     decoration: TextDecoration.underline,
                   ),),
               )
+
             ],
           )
         ],
